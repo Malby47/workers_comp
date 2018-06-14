b7d1a3ea75d69cab7f2fa60c5579c281ca14935b
Liam Malby <liam.malby@gmail.com>
2018-06-14 21:32:49 +1000

    Removing venv

--- __init__.py
+++
@@ -1,9 +0,0 @@
-
-# import modules
-
-import numpy as np
-import pandas as pd
-
-# import data
-filename = 'raw_data/20140701OFSWQ_WorkersCompensation.csv'
-data = pd.read_csv(filename)
---
+++ initial_import.py
@@ -0,0 +1,8 @@
+# import modules
+
+import numpy as np
+import pandas as pd
+
+# import data
+filename = 'raw_data/20140701OFSWQ_WorkersCompensation.csv'
+data = pd.read_csv(filename)
